7bea71632b0e55f498ec947a94026485a7266669
Saher <Saher@email.com>
2024-03-01 20:28:23 +0200

    The SUM function has updated to accept 3 numbers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-let FirstNum = 1;
-let SecondNum = 2;
+let FirstNum = 1,
+  SecondNum = 2,
+  ThirdNum = 3;
 
-function Sum(num1, num2) {
-  console.log(num1 + num2);
-  return num1 + num2;
+function Sum(num1, num2, num3) {
+  console.log(num1 + num2 + num3);
+  return num1 + num2 + num3;
 }
 
-Sum(FirstNum, SecondNum);
+Sum(FirstNum, SecondNum, ThirdNum);
